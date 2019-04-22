ed47239a8725cda6412c24f85cccb914ebc7998b
Xiaoquan Kong <u1mail2me@gmail.com>
2019-04-22 21:23:10 +0800

    Add v0.1 code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rasa_nlu_addons',
+    version='0.0.1',
+    packages=find_packages(),
+    url='',
+    license='Apache 2.0',
+    author='Xiaoquan Kong',
+    author_email='u1mail2me@gmail.com',
+    description='Addons for Rasa NLU'
+)
