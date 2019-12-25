146dff8ddc8587c0e2bbb2d17354a8c4c62ed969
Xiaoquan Kong <u1mail2me@gmail.com>
2019-12-25 15:46:12 +0800

    typofix

--- rasa_contrib/nlu/featurizers/base_featurizer.py
+++ rasa_contrib/nlu/featurizers/base_featurizer.py
@@ -1,4 +1,9 @@
-from rasa.nlu.featurizers.featurizer import Featurizer
+try:
+    # form rasa 1.6.x
+    from rasa.nlu.featurizers.featurizer import Featurizer
+except ImportError:
+    # for rasa 1.5.x
+    from rasa.nlu.featurizers import Featurizer
 
 
 class ContribFeaturizer(Featurizer):
