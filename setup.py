017d58d4963531d56ae4508aa200b9eb9113516f
Xiaoquan Kong <u1mail2me@gmail.com>
2020-07-09 13:53:28 +0800

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rasa_contrib',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     url='https://github.com/howl-anderson/rasa_contrib',
     license='Apache 2.0',
